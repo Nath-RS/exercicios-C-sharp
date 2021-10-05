ba7b16d8778f5ad7bf5801bc303c45e3bc53b4e0
Nath-RS <78105588+Nath-RS@users.noreply.github.com>
2021-10-05 16:25:52 -0300

    Update 05/10/2021

--- URI 1045/URI 1045/Program.cs
+++ URI 1045/URI 1045/Program.cs
@@ -7,6 +7,8 @@ namespace URI_1045
         static void Main(string[] args)
         {
             float A=0, B=0, C=0;
+            bool forma = true;
+
             
             string[] input = Console.ReadLine().Split();
             float[] numeros = new float[input.Length];
@@ -16,37 +18,29 @@ namespace URI_1045
                 numeros[i] = float.Parse(input[i]);
             }
 
-            if (numeros[0] >= numeros[1] && numeros[0] >= numeros[2]) A = numeros[0];
-            else
-            if (numeros[1] >= numeros[0] && numeros[1] >= numeros[2]) A = numeros[1];
-            else
-            if (numeros[2] >= numeros[1] && numeros[2] >= numeros[0]) A = numeros[2];
-
-            for (int i = 0; i < numeros.Length; i++) 
+            if (numeros[0] >= numeros[1] && numeros[0] >= numeros[2])
             {
-                if (A != numeros[i] && B == 0)
-                {
-                    B = numeros[i];
-                }
+                A = numeros[0]; B = numeros[1]; C = numeros[2];
             }
-            for (int i = 0; i < numeros.Length; i++)
+            else if (numeros[1] >= numeros[0] && numeros[1] >= numeros[2])
             {
-                if (B != numeros[i] && C == 0)
-                {
-                    C = numeros[i];
-                }
+                A = numeros[1]; B = numeros[0]; C = numeros[2];
+            }
+            else if (numeros[2] >= numeros[1] && numeros[2] >= numeros[0])
+            {
+                A = numeros[2]; B = numeros[0]; C = numeros[1];
             }
 
-            Console.WriteLine(A + " " + B + " " + C);
+            if (A >= C + B)
+            {
+                Console.WriteLine("NAO FORMA TRIANGULO"); forma = false;
+            }
+            if (forma == true && A * A == C * C + B * B) Console.WriteLine("TRIANGULO RETANGULO");
+            if(forma == true && A * A > C * C + B * B) Console.WriteLine("TRIANGULO OBTUSANGULO");
+            if (forma == true && A * A < C * C + B * B) Console.WriteLine("TRIANGULO ACUTANGULO");
+            if (forma == true && A == C && A == B) Console.WriteLine("TRIANGULO EQUILATERO");
+            if (forma == true && A == B && A != C || A == C && A != B || B == C && B != A) Console.WriteLine("TRIANGULO ISOSCELES");
             
-            /*
-            if (A >= C + B) Console.WriteLine("NAO FORMA TRIANGULO");
-            if (A * A == C * C + B * B) Console.WriteLine("TRIANGULO RETANGULO");
-            if (A * A > C * C + B * B) Console.WriteLine("TRIANGULO OBTUSANGULO");
-            if (A * A < C * C + B * B) Console.WriteLine("TRIANGULO ACUTANGULO");
-            if (A == C && A == B) Console.WriteLine("TRIANGULO EQUILATERO");
-            if (A == B && A != C || A == C && A != B) Console.WriteLine("TRIANGULO ISOSCELES");
-            */
         }
     }
 }
