332ac31fef57543e1258496c266bc9096c8505db
Nath-RS <nathandesouzarodrigues@gmail.com>
2022-02-08 16:09:51 -0300

    update janeiro

--- Revisao/exercicio 4/Program.cs
+++ Revisao/exercicio 4/Program.cs
@@ -10,6 +10,21 @@ namespace exercicio_4
     {
         static void Main(string[] args)
         {
+            string numerorg,nome,email,telefone;
+            Console.WriteLine("Digite seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite seu RG: ");
+            numerorg = Console.ReadLine();
+
+            Console.WriteLine("Digite seu Telefone: ");
+            telefone = Console.ReadLine();
+
+            Console.WriteLine("Digite seu email: ");
+            email = Console.ReadLine(); 
+
+
+
         }
     }
 }
